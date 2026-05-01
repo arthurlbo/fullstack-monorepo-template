a2a70c040d25ffecd9b696b9aedf290f190bdb25
arthurlbo <arthurlbo16@gmail.com>
2026-05-01 00:50:43 -0300

    chore(packages): remove @repo/database-typeorm

--- packages/database-typeorm/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { baseConfig } from "@repo/tsup";
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    ...baseConfig,
-    entry: ["src/index.ts"],
-    bundle: true,
-    external: ["typeorm"],
-});
